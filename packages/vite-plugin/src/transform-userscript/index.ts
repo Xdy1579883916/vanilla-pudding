aa19963394f8a6dbf346d5638f308a56242cecfb
deyu <1579883916@qq.com>
2024-06-25 10:28:43 +0800

    增加 vite-plugin, 抽离 transformUserScript, 提供友好的ts 类型提示;

---
+++ packages/vite-plugin/src/transform-userscript/index.ts
@@ -0,0 +1,77 @@
+type StrOrStrArr = string | string[];
+
+interface ScriptMeta {
+  "name": string;
+  "world"?: chrome.userScripts.ExecutionWorld;
+  "runAt"?: chrome.userScripts.RunAt;
+  "run-at"?: chrome.userScripts.RunAt;
+  "allFrames"?: boolean;
+  "all-frames"?: boolean;
+  "match"?: StrOrStrArr;
+  "exclude-match"?: StrOrStrArr;
+  "excludeMatch"?: StrOrStrArr;
+  "exclude-glob"?: StrOrStrArr;
+  "excludeGlob"?: StrOrStrArr;
+  "include-glob"?: StrOrStrArr;
+  "includeGlob"?: StrOrStrArr;
+  "update-url"?: StrOrStrArr;
+  "updateUrl"?: StrOrStrArr;
+}
+
+interface Options {
+  isDev: boolean,
+  cdnScripts: Record<string, string>,
+  scriptMeta: ScriptMeta
+}
+
+export function transformUserScript({cdnScripts, scriptMeta}: Options) {
+  const name = "transformUserScript";
+  // 将metaStr计算移到generateBundle外部以优化性能
+  let metaStr = generateMetaStr(scriptMeta);
+
+  return {
+    name,
+    enforce: "post",
+    async renderChunk(code: string, meta: { type: string }) {
+      if (meta.type === "chunk") {
+        // 替换import的相对路径和动态导入
+        let newCode = replaceImports(code, cdnScripts);
+        return {
+          code: newCode,
+        };
+      }
+      return {code};
+    },
+    async generateBundle(_, bundle) {
+      for (const fileName in bundle) {
+        const chunk = bundle[fileName];
+        // 直接使用预先计算的metaStr
+        chunk.code = metaStr + chunk.code;
+      }
+    },
+    closeBundle() {
+      console.log(`[Plugin] ${name} -> ending～`);
+    },
+  };
+}
+
+function replaceImports(code: string, cdnScripts: Record<string, string>): string {
+  let newCode = code;
+  Object.entries(cdnScripts).forEach(([scriptName, cdnURL]) => {
+    newCode = newCode.replace(new RegExp(`(import|from)\\s?["'](${scriptName})["'];`, "gi"), `$1 "${cdnURL}";`)
+  });
+  return newCode;
+}
+
+function generateMetaStr(scriptMeta: ScriptMeta): string {
+  return Object.entries(scriptMeta).reduce((pre, [k, v]) => {
+    if (Array.isArray(v)) {
+      v.forEach((item) => {
+        pre += `// @${k} ${item}\n`;
+      });
+    } else {
+      pre += `// @${k} ${v}\n`;
+    }
+    return pre;
+  }, "");
+}
--- project/vpu-test/vite.config.js
+++ project/vpu-test/vite.config.js
@@ -1,49 +1,50 @@
 import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
-import {transformUserScriptPlugin} from "./vite-plugin/transformUserScriptPlugin.js";
+import {transformUserScript} from "@vanilla-pudding/vite-plugin";
+
 
 // https://vitejs.dev/config/
 export default defineConfig(({mode}) => {
-    const isDev = mode === 'development';
-    const cdnScripts = {
-        "vue": "https://js-c.etc4.com/pro/js/vue.MCL5DJ5O.js",
-        "naive-ui": "https://js-c.etc4.com/pro/js/naive-ui.4HRUQBDM.js",
-        "@vanilla-pudding/message": "https://unpkg.com/@vanilla-pudding/message/dist/index.js",
-    }
+  const isDev = mode === 'development';
+  const cdnScripts = {
+    "vue": "https://js-c.etc4.com/pro/js/vue.MCL5DJ5O.js",
+    "naive-ui": "https://js-c.etc4.com/pro/js/naive-ui.4HRUQBDM.js",
+    "@vanilla-pudding/message": "https://unpkg.com/@vanilla-pudding/message/dist/index.js",
+  }
 
-    return {
-        server: {
-            port: 5177
+  return {
+    server: {
+      port: 5177
+    },
+    plugins: [
+      vue(),
+      transformUserScript({
+        scriptMeta: {
+          name: "消息通信测试",
+          match: "*://baidu.com/*",
+          allFrames: true,
+          runAt: "document_start",
         },
-        plugins: [
-            vue(),
-            transformUserScriptPlugin({
-                isDev,
-                scriptMeta: {
-                    name: "消息通信测试",
-                    match: "*://baidu.com/*",
-                    allFrames: "true",
-                },
-                cdnScripts,
-            })
-        ],
-        build: {
-            modulePreload: false,
-            minify: "terser",
-            terserOptions: {
-                compress: {
-                    drop_console: true,
-                    drop_debugger: true,
-                    pure_funcs: ["console.log"],
-                },
-            },
-            rollupOptions: {
-                output: {
-                    entryFileNames: "[name].js",
-                    format: "esm",
-                },
-                external: Object.keys(cdnScripts)
-            }
-        }
+        cdnScripts,
+      })
+    ],
+    build: {
+      modulePreload: false,
+      minify: "terser",
+      terserOptions: {
+        compress: {
+          drop_console: true,
+          drop_debugger: true,
+          pure_funcs: ["console.log"],
+        },
+      },
+      rollupOptions: {
+        output: {
+          entryFileNames: "[name].js",
+          format: "esm",
+        },
+        external: Object.keys(cdnScripts)
+      }
     }
+  }
 })
